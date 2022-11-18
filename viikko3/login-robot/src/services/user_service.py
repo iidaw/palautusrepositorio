58cc6cc4273e01570b112c8ccc087297bd5024bf
Iida Björn <iida.w.bjorn@gmail.com>
2022-11-18 13:27:55 +0200

    teht 4-7

---
+++ viikko3/login-robot/src/services/user_service.py
@@ -0,0 +1,50 @@
+from entities.user import User
+import re
+
+
+class UserInputError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self._user_repository = user_repository
+
+    def check_credentials(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        user = self._user_repository.find_by_username(username)
+
+        if not user or user.password != password:
+            raise AuthenticationError("Invalid username or password")
+
+        return user
+
+    def create_user(self, username, password):
+        self.validate(username, password)
+
+        user = self._user_repository.create(
+            User(username, password)
+        )
+
+        return user
+
+    def validate(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+
+        if len(username) < 3:
+            raise UserInputError("Username too short")
+
+        if len(password) < 8:
+           raise UserInputError("Password too short")
+
+        if re.search('[0-9]', password) is None:
+            raise UserInputError("Password doesn't contain numbers")
